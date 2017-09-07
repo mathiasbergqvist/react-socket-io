8cdfce6156b5b7502a81b0e8f1e3d8b48ab5ee0a
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2017-09-07 20:06:29 +0200

    Setup socket.io on the server side

--- react-socket-io/server.js
+++ react-socket-io/server.js
@@ -1,5 +1,15 @@
 const io = require('socket.io')();
 
+const port = 8099;
+io.listen(port);
+console.log("listerning on port", port);
+
 io.on('connection', (client) => {
   //This is where we emit events to the client
+  client.on('subscribeToTimer', () => {
+    console.log('client is subscribing to timer with interval', interval);
+    setInterval(() => {
+      client.emit('timer', new Date());
+    }, interval);
+  });
 });
