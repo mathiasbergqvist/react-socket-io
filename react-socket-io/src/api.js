73a65454a335c91e3e9a62bffe4df18f65b238b2
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2017-09-07 20:18:28 +0200

    Setup socket.io on the server side

---
+++ react-socket-io/src/api.js
@@ -0,0 +1,10 @@
+import openSocket from 'socket.io-client';
+
+const socket = openSocket('http://localhost:8099');
+
+const subscribeToTimer = (callback) => {
+  socket.on('timer', => timestamp => callback(null, timestamp));
+  socket.emit('subscribeToTimer', 1000);
+};
+
+export {subscribeToTimer};
